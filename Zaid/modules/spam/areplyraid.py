13658c3195b094eac10e7f66b93508f949a74048
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-21 08:40:37 +0530

    Update areplyraid.py

--- Zaid/modules/spam/areplyraid.py
+++ Zaid/modules/spam/areplyraid.py
@@ -3,12 +3,12 @@ import sys
 import asyncio
 import re
 from random import choice
-from pyrogram import errors, Client, filters, 
+from pyrogram import Client, filters
 from pyrogram.types import Message
 from cache.data import *
 from Zaid.database.rraid import *
 from Zaid import SUDO_USER
-from pyrogram import errors
+from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
 DEVS = int(7374966263)
 from Zaid.helper.PyroHelpers import get_ub_chats
@@ -16,42 +16,40 @@ from Zaid.modules.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
 from .replyraid import RAIDS
 
+
+
 @Client.on_message(
-    filters.command(["rraid"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["replyraid"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def gmute_user(client: Client, message: Message):
     args = await extract_user(message)
     reply = message.reply_to_message
-    
-    ex = await message.reply_text("`Processing...`")  # Changed to reply_text to create a new message
-    
+    ex = await message.edit_text("`Processing...`")
     if args:
         try:
             user = await client.get_users(args)
         except Exception:
-            await ex.edit_text(f"`Please specify a valid user!`")
+            await ex.edit(f"`Please specify a valid user!`")
             return
     elif reply:
         user_id = reply.from_user.id
         user = await client.get_users(user_id)
     else:
-        await ex.edit_text(f"`Please specify a valid user!`")
+        await ex.edit(f"`Please specify a valid user!`")
         return
-
     if user.id == client.me.id:
-        return await ex.edit_text("**Okay Sure.. 🐽**")
+        return await ex.edit("**Okay Sure.. 🐽**")
     elif user.id == SUDO_USERS:
-        return await ex.edit_text("**Okay But Failed Because this user in sudos.. 🐽**")
+        return await ex.edit("**Okay But Failed Because this user in sudos.. 🐽**")
     elif user.id == VERIFIED_USERS:
-        return await ex.edit_text("**Chal Chal Baap ko Mat sikha.. 🐽**")
-    
+        return await ex.edit("**Chal Chal Baap ko Mat sikha.. 🐽**")
     try:
         if user.id in (await get_rraid_users()):
-            await ex.edit_text("Replyraid is activated on this user")
-            return
+           await ex.edit("Replyraid is activated on this user")
+           return
         await rraid_user(user.id)
         RAIDS.append(user.id)
-        await ex.edit_text(f"[{user.first_name}](tg://user?id={user.id}) Activated ReplyRaid!")
+        await ex.edit(f"[{user.first_name}](tg://user?id={user.id}) Activated ReplyRaid!")
     except Exception as e:
-        await ex.edit_text(f"**ERROR:** `{e}`")
+        await ex.edit(f"**ERROR:** `{e}`")
         return
